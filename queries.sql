bf6ece9afbe7a6f0f093ccc280f1d3c6419b7bdf
Christina Harris <christinathedesigner@outlook.com>
2020-05-12 09:23:58 -0700

    forgot something

--- queries.sql
+++ queries.sql
@@ -7,6 +7,11 @@ JOIN "Category" as c
 ON p.CategoryId = c.Id
 
 -- Display the order Id and shipper CompanyName for all orders placed before August 9 2012. Shows 429 records.
+SELECT o.Id, s.CompanyName
+FROM "Order" as o
+JOIN Shipper as s
+ON o.ShipVia = s.Id
+WHERE o.OrderDate < "2012-08-09"
 
 -- Display the name and quantity of the products ordered in order with Id 10251. Sort by ProductName. Shows 3 records.
 SELECT p.ProductName, o.Quantity
